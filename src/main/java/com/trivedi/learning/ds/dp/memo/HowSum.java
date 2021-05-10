4daba066739d5f886d91854161fa4a4b50cada3c
Trivedi B <theepi1980@Trivedis-MacBook-Pro.local>
2021-05-10 02:12:42 -0500

    added DP memo algo

---
+++ src/main/java/com/trivedi/learning/ds/dp/memo/HowSum.java
@@ -0,0 +1,66 @@
+package com.trivedi.learning.ds.dp.memo;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class HowSum {
+    public static void main(String[] args) {
+        Map<Integer, List<Integer>> memo = new HashMap<>();
+        System.out.println(howSum(new int[] { 2, 3}, 7, memo));
+        memo.clear();
+        System.out.println(howSum(new int[] { 5, 3, 4, 7}, 7, memo));
+        memo.clear();
+        System.out.println(howSum(new int[] { 2, 4}, 7, memo));
+        memo.clear();
+        System.out.println(howSum(new int[] { 7, 14}, 600, memo));
+        memo.clear();
+
+        System.out.println(bestSum(new int[] { 2, 3, 7}, 7, memo));
+        memo.clear();
+        System.out.println(bestSum(new int[] { 2, 3, 5}, 8, memo));
+        memo.clear();
+        System.out.println(bestSum(new int[] { 1, 4, 5}, 8, memo));
+        memo.clear();
+    }
+
+    private static List<Integer> howSum(int[] arr, int target, Map<Integer, List<Integer>> memo) {
+        if (memo.containsKey(target)) return memo.get(target);
+        if (arr.length <= 0 || target < 0) return null;
+        if (target == 0) return new ArrayList<>();
+
+        for (int value : arr) {
+            if (value > target) continue;
+
+            List<Integer> list = howSum(arr, target - value, memo);
+            if (list != null) {
+                list.add(value);
+                memo.put(target, list);
+                return list;
+            }
+        }
+        memo.put(target, null);
+        return null;
+    }
+
+    private static List<Integer> bestSum(int[] arr, int target, Map<Integer, List<Integer>> memo) {
+        if (arr.length <= 0 || target < 0) return null;
+        if (target == 0) return new ArrayList<>();
+        if (memo.containsKey(target)) return memo.get(target);
+
+        List<Integer> bestOne = null;
+        for (int value : arr) {
+            List<Integer> list = bestSum(arr, target - value, memo);
+            if (list != null) {
+                if (bestOne == null || bestOne.size() > list.size()) {
+                    bestOne = new ArrayList<>(list);
+                    bestOne.add(value);
+                }
+            }
+        }
+        // System.out.println("target: " + target + " - > " + bestOne);
+        memo.put(target, bestOne);
+        return bestOne;
+    }
+}
