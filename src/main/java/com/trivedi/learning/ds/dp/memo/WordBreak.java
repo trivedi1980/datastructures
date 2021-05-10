87251472dad19fde8fd32db84b9a7a6862630279
Trivedi B <theepi1980@Trivedis-MacBook-Pro.local>
2021-05-10 13:34:41 -0500

    added word break count method

--- src/main/java/com/trivedi/learning/ds/dp/memo/WordBreak.java
+++ src/main/java/com/trivedi/learning/ds/dp/memo/WordBreak.java
@@ -5,26 +5,40 @@ import java.util.*;
 public class WordBreak {
     public static void main(String[] args) {
         Map<String, Boolean> memo = new HashMap<>();
+        Map<String, Integer> memo2 = new HashMap<>();
 
         List<String> wordDict = new ArrayList<>();
         wordDict.add("leet");
         wordDict.add("code");
+        wordDict.add("le");
+        wordDict.add("et");
         System.out.println(wordBreak("leetcode", wordDict, memo));
+        System.out.println(countWordBreak("leetcode", wordDict, memo2));
 
-        wordDict.clear();;
+        wordDict.clear();
+        memo.clear();
+        memo2.clear();
         wordDict.add("car");
         wordDict.add("ca");
         wordDict.add("rs");
         System.out.println(wordBreak("cars", wordDict, memo));
+        System.out.println(countWordBreak("cars", wordDict, memo2));
 
         String[] words = new String[] {"a","aa","aaa","aaaa","aaaaa","aaaaaa","aaaaaaa","aaaaaaaa",
                 "aaaaaaaaa","aaaaaaaaaa"};
         wordDict.clear();
+        memo.clear();
+        memo2.clear();
         wordDict.addAll(Arrays.asList(words));
         System.out.println(wordBreak("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
                         "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
                         "aaaaaaaaaaaaab",
                 wordDict, memo));
+        System.out.println(countWordBreak("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+                        "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+                        "aaaaaaaaaaaaab",
+                wordDict, memo2));
+
     }
 
     private static boolean wordBreak(String s, List<String> wordDict, Map<String, Boolean> memo) {
@@ -39,5 +53,19 @@ public class WordBreak {
         }
         memo.put(s, false);
         return false;
+    }
+
+    private static int countWordBreak(String s, List<String> wordDict, Map<String, Integer> memo) {
+        if (s == null || s.isEmpty()) return 1;
+        if (memo.containsKey(s)) return memo.get(s);
+        int count = 0;
+        for (String word : wordDict) {
+            int index = s.indexOf(word);
+            if (index == 0) {
+                count += countWordBreak(s.substring(word.length()), wordDict, memo);
+            }
+        }
+        memo.put(s, count);
+        return count;
     }
 }
